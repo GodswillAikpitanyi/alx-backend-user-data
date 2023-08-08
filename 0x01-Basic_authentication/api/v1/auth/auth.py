d2081c6a847dc76898fd2841b085ec436e215b85
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2023-08-08 21:12:40 +0000

    authentication routes updates

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,7 +14,15 @@ class Auth:
             -path
             -excluded path
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or excluded_paths == []:
+            return True
+        if not path.endswith("/"):
+            path += "/"
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
