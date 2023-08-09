405f8d3fb869f0ad2d1a0f4db1d97e7820223ab7
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2023-08-09 13:55:42 +0000

    reqiure auth with stars

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -20,8 +20,11 @@ class Auth:
             return True
         if not path.endswith("/"):
             path += "/"
-        if path in excluded_paths:
-            return False
+        '''if path in excluded_paths:
+            return False'''
+        for e_path in excluded_paths:
+            if path.find(e_path) != -1:
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
