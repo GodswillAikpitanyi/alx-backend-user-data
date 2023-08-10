12e59644de5bf710f67f156e591ed8cb08e787d2
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2023-08-10 14:36:01 +0000

    User ID for Session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -19,5 +19,15 @@ class SessionAuth(Auth):
         if not isinstance(user_id, str):
             return None
         s_id = str(uuid4())
-        SessionAuth.user_id_by_session_id[s_id] = user_id
+        self.user_id_by_session_id[s_id] = user_id
         return s_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        '''
+        returns a User ID based on a Session ID
+        '''
+        if session_id is None:
+            return None
+        if not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
