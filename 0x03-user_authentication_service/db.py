0303a589857282491b00305a18b27ac90dc55101
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2023-08-15 20:04:10 +0000

    User creation

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,56 @@
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        adds user to DB
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        returns a user
+        """
+        users_all = self._session.query(User)
+        for key, value in kwargs.items():
+            if key not in User.__dict__:
+                raise NoResultFound
+
+
+        for key, value in users_all:
+            if getattr(users_all, key) == value:
+                return users_all
+        return InvalidRequestError
