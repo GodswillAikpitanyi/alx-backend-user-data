d55e7e2cb054ed47810fdf82c35a201a4b08eec0
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2023-08-16 16:59:43 +0000

    Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Flask app
 """
-from flask import Flask, jsonify, make_response, request
+from flask import abort, Flask, jsonify, make_response, request
 from auth import Auth
 
 
@@ -30,6 +30,24 @@ def users():
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
     return jsonify({"email": f"{email}", "message": "user created"})
+
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    """
+    login in
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": f"{email}", "message": "logged in"})
+    response = make_response(response)
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 if __name__ == "__main__":
