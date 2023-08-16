2d88c7da5ab13f6a2cf4ead71a528248f7eb853c
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2023-08-16 20:23:19 +0000

    Destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -80,3 +80,10 @@ class Auth:
             return None
 
         return user
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+        Destroy session
+        """
+        return self._db.update_user(user_id, session_id=None)
+        
