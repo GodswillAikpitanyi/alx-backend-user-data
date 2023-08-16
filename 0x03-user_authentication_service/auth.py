8ccb548ee5004f38a4e3b6928c9d99d29d95c266
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2023-08-16 15:59:44 +0000

    Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -38,7 +38,7 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-        except NoResultFound:
+        except Exception:
             return False
         if user:
             password = password.encode('utf-8')
