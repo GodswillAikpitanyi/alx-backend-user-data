3dcb619258a8b5ae45c312038efda1940eafb434
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2023-08-17 13:01:11 +0000

    Generate reset password token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -91,3 +91,16 @@ class Auth:
             user = self._db.update_user(user_id, session_id=None)
         except ValueError:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        Generates reset password token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
